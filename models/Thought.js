86ed7f490c59abd398a796d97b009b1e8207c570
Sam Miller <samdakota@gmail.com>
2022-06-20 08:48:19 -0400

    added Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,57 @@
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reatctionText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 const ThoughtSchema = new Schema(
     {
-        
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        }
     }
-)
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
+
